61b006e5c9e3118082c9c4304748d8fcd7194173
sbaue <130162932+sbaue@users.noreply.github.com>
2023-04-15 19:26:19 +0200

    Fix for rpm changes with multiple rpms

--- build.sh
+++ build.sh
@@ -4,10 +4,16 @@ RPM_OL=$(cat /tmp/rpm_overlay | egrep -v "(^#.*|^$)")
 RPM_IN=$(cat /tmp/rpm_install | egrep -v "(^#.*|^$)")
 
 echo "-- Applying overlay as defined in rpm_overlay --"
-    rpm-ostree override remove $RPM_OL
+for pkg-out in $(echo -e "$RPM_OL"); do \
+    echo "Removing: ${pkg-out}" && \
+    rpm-ostree override remove $pkg; \
+done
+
 echo "---"
 
 echo "-- Applying layering as defined in rpm_install --"
-    echo "Installing: $RPM_IN"
-    rpm-ostree install $RPM_IN
+for pkg-in in $(echo -e "$RPM_IN"); do \
+    echo "Installing: ${pkg-in}" && \
+    rpm-ostree install $pkg; \
+done
 echo "---"
