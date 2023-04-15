10f37ea9d4b24b0301d59904b05b206145a08fe9
sbaue <130162932+sbaue@users.noreply.github.com>
2023-04-15 19:30:27 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ RPM_OL=$(cat /tmp/rpm_overlay | egrep -v "(^#.*|^$)")
 RPM_IN=$(cat /tmp/rpm_install | egrep -v "(^#.*|^$)")
 
 echo "-- Applying overlay as defined in rpm_overlay --"
-for pkg-out in $(echo -e "$RPM_OL"); do \
-    echo "Removing: ${pkg-out}" && \
+for pkg in $(echo -e "$RPM_OL"); do \
+    echo "Removing: ${pkg}" && \
     rpm-ostree override remove $pkg; \
 done
 
 echo "---"
 
 echo "-- Applying layering as defined in rpm_install --"
-for pkg-in in $(echo -e "$RPM_IN"); do \
-    echo "Installing: ${pkg-in}" && \
+for pkg in $(echo -e "$RPM_IN"); do \
+    echo "Installing: ${pkg}" && \
     rpm-ostree install $pkg; \
 done
 echo "---"
