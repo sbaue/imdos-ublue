32fb59dec646b3e30c08dcbb9aa943bea3ba4e88
sbaue <sbaue@users.noreply.github.com>
2023-04-15 20:15:16 +0200

    This might work better edition
    We need to install packages on one line due to dependency handling of rpm-ostree

--- build.sh
+++ build.sh
@@ -3,17 +3,15 @@
 RPM_OL=$(cat /tmp/rpm_overlay | egrep -v "(^#.*|^$)")
 RPM_IN=$(cat /tmp/rpm_install | egrep -v "(^#.*|^$)")
 
-echo "-- Applying overlay as defined in rpm_overlay --"
-for pkg in $(echo -e "$RPM_OL"); do \
-    echo "Removing: ${pkg}" && \
-    rpm-ostree override remove $pkg; \
-done
-
 echo "---"
 
+echo "-- Applying overlay as defined in rpm_overlay --"
+echo "Removing: $RPM_OL" 
+rpm-ostree override remove $RPM_OL
+
+
 echo "-- Applying layering as defined in rpm_install --"
-for pkg in $(echo -e "$RPM_IN"); do \
-    echo "Installing: ${pkg}" && \
-    rpm-ostree install $pkg; \
-done
+echo "Installing: $RPM_IN"
+rpm-ostree install $RPM_IN
+
 echo "---"
